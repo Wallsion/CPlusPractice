8e1fb264185c29b988e83c63d5717b189e719148
Wangxin <33243170+Wallsion@users.noreply.github.com>
2018-11-13 22:23:30 +0800

    Create maxLenSubstr.cpp
    Longest Substring Without Repeating Characters

---
+++ leetcode/maxLenSubstr.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        std::array<int,200> temArray;
+        for(auto it = temArray.begin(); it != temArray.end(); ++it)
+        {
+            *it = 0;
+        }
+        int pre = 0;
+        int next = 0;
+        int max = 0;
+        if(s.length() == 1)
+            return 1;
+        else if(s.length() == 0)
+            return 0;
+        else
+        {
+            while(next != s.length())
+            {
+                if(temArray[s[next]] < 1)
+                    temArray[s[next]] = temArray[s[next]] + 1;
+                else
+                {
+                    temArray[s[next]] = temArray[s[next]] + 1;
+                    if(max < next - pre)
+                            max = next - pre;
+                    for(int i = pre; i < next; ++i)
+                    {
+                        if(s[i] != s[next])
+                            temArray[s[i]] = temArray[s[i]] - 1;
+                        else
+                        {
+                            temArray[s[i]] = temArray[s[i]] - 1;
+                            pre = i + 1;
+                            break;
+                        }
+                    }
+                }
+               ++next;
+            }
+        }
+        return max > next - pre ? max : next - pre;
+    }
+};
